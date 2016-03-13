f4ff3256551302022756bb814cdae97f61595f48
jahepi <jahepi@gmail.com>
2016-03-12 22:38:22 -0600

    app changes ...

--- app/src/main/java/pennsylvania/jahepi/com/apppenns/activities/CalendarActivity.java
+++ app/src/main/java/pennsylvania/jahepi/com/apppenns/activities/CalendarActivity.java
@@ -33,27 +33,28 @@ public class CalendarActivity extends AuthActivity implements View.OnClickListen
         Button backBtn = (Button) findViewById(R.id.backBtn);
         backBtn.setOnClickListener(this);
 
-        CaldroidFragment caldroidFragment = new CaldroidFragment();
-        Bundle args = new Bundle();
         Calendar cal = Calendar.getInstance();
-        args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
-        args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
-        caldroidFragment.setArguments(args);
-
-        android.support.v4.app.FragmentTransaction t = getSupportFragmentManager().beginTransaction();
-        t.replace(R.id.calendarFragment, caldroidFragment);
-        t.commit();
-
         Intent intent = getIntent();
         String date = intent.getStringExtra(CustomApplication.GENERIC_INTENT);
         if (date != null) {
             SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
             try {
-                caldroidFragment.setSelectedDate(dateFormat.parse(date));
+                cal.setTime(dateFormat.parse(date));
             } catch (ParseException e) {
                 e.printStackTrace();
             }
         }
+
+        CaldroidFragment caldroidFragment = new CaldroidFragment();
+        Bundle args = new Bundle();
+        args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
+        args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
+        caldroidFragment.setArguments(args);
+        caldroidFragment.setSelectedDate(cal.getTime());
+
+        android.support.v4.app.FragmentTransaction t = getSupportFragmentManager().beginTransaction();
+        t.replace(R.id.calendarFragment, caldroidFragment);
+        t.commit();
 
         caldroidFragment.setCaldroidListener(new CaldroidListener() {
             @Override
